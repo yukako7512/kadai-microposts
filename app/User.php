0c795cf01c831843113e4e248dfae7cb585862de
yukako.nishiyama <yukako.1995@gmail.com>
2018-06-12 03:58:36 +0000

    count microposts

--- app/User.php
+++ app/User.php
@@ -32,6 +32,13 @@ class User extends Authenticatable
         return $this->hasMany(Micropost::class);
     }
     
+    
+    
+    
+    
+
+    
+    
      public function followings()
     {
         return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
